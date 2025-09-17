cfed2667ec36e90de0c5e45c41160ce2937d643b
caiohperlin <caiohperlin@gmail.com>
2025-09-16 20:16:38 -0400

    feat: add onclick confetti event

--- script.js
+++ script.js
@@ -3,18 +3,35 @@ const jsConfetti = new JSConfetti();
 document.addEventListener('DOMContentLoaded', () => {
     jsConfetti.addConfetti({
         emojis: ['🐺', '🐷'],
-        emojiSize: 80,
+        emojiSize: 40,
         confettiNumber: 40,
     })
 });
 
 const emojiElements = document.querySelectorAll('.emoji');
 emojiElements.forEach(el => {
-    el.addEventListener('click', () => {
-        jsConfetti.addConfetti({
-            emojis: [el.textContent],
-            emojiSize: 100,
-            confettiNumber: 20,
+    el.addEventListener('click', (event) => {
+        const trimmedContent = el.textContent.trim();
+        const isWolf = trimmedContent === '🐺';
+        const isPig = trimmedContent === '🐷';
+
+        let emojiToEmit = trimmedContent;
+        const emojiSize = 80;
+        const confettiNumber = 20;
+
+        if (isWolf) emojiToEmit = '💥';
+        else if (isPig) emojiToEmit = '🥓';
+
+        jsConfetti.addConfettiAtPosition({
+            emojis: [emojiToEmit],
+            emojiSize: emojiSize,
+            confettiNumber: confettiNumber,
+            confettiDispatchPosition: {
+                x: event.clientX,
+                y: event.clientY
+            }
         });
+
+        if (isWolf || isPig) el.textContent = '🪦';
     });
 });
